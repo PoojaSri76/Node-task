09b6976ddb5a1860d49cfb061b8c59207dd6a850
Pooja <pooja76212@gmail.com>
2025-11-05 12:33:36 +0530

    day2 task completed

--- day2/FileSystem.js
+++ day2/FileSystem.js
@@ -1,12 +1,12 @@
 import { log } from 'console';
 import fs from 'fs';
 
-// fs.writeFile("sample.txt","Hello",(err)=>{
-//     if (err) {
-//         console.log(err);
+fs.writeFile("sample.txt","Hello",(err)=>{
+    if (err) {
+        console.log(err);
         
-//     }
-// })
+    }
+})
 
 // const data = fs.readFileSync("sample.text",(err)=>{
 //      if (err) {
@@ -17,27 +17,20 @@ import fs from 'fs';
     
 // })
 
-// fs.appendFile("sample.txt", "\n Thank you",(err)=>{
-//     if (err) {
-//         console.log(err);
-        
-//     }
-//     console.log("data added successfully");
-    
-// })
-
-// fs.rename("sample.txt", "demo.txt", (err)=>{
-//     if (err) {
-//         console.log(err);
-        
-//     }
-//     console.log("Renamed");
-    
-// })
-
-fs.writeFile("sample.txt","Hello",(err)=>{
+fs.appendFile("sample.txt", "\n Thank you",(err)=>{
     if (err) {
         console.log(err);
         
     }
+    console.log("data added successfully");
+    
+})
+
+fs.rename("sample.txt", "demo.txt", (err)=>{
+    if (err) {
+        console.log(err);
+        
+    }
+    console.log("Renamed");
+    
 })
